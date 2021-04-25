120dc28d7a5a0efafe3092e264bef80f44279b70
shinbeomsoo <sdalbsoo@gmail.com>
2021-04-26 01:37:18 +0900

    update 스테픈커리 데이터 csv파일 만들어보기

--- stephen-curry/README.md
+++ stephen-curry/README.md
@@ -1,4 +1,5 @@
 # 스테판 커리 Stephen Curry
 
-
-
+ref.
+- https://www.nba.com/player/201939/stephen-curry
+- https://www.basketball-reference.com/players/c/curryst01/gamelog/2021
--- stephen-curry/stephen_curry_data.py
+++ stephen-curry/stephen_curry_data.py
@@ -1,31 +1,43 @@
-from pprint import pprint
+import csv
+from typing import Any, Dict
 
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
+from requests import Response
 
 
-curry_profile = requests.get(
-    "https://www.basketball-reference.com/players/c/curryst01/gamelog/2021"
-)
-
-def get_stat_data(data_stat: str):
-    result = []
+def get_stat_data(curry_profile: Response) -> Dict[str, Any]:
+    stat_data = {}
+    stats = ["pts", "fg3", "fg"]
     html = curry_profile.text
     soup = BeautifulSoup(html, "html.parser")
-    points = soup.find_all(attrs={"data-stat": data_stat})
-    for point in points:
-        try:
-            result.append(int(point.string))
-        except ValueError:
-            pass
-    return result
+    for stat in stats:
+        points = soup.find_all(attrs={"data-stat": stat})
+        total_stat = []
+        for point in points:
+            try:
+                total_stat.append(int(point.string))
+            except ValueError:
+                pass
+        stat_data[stat] = total_stat
+    return stat_data
 
 
+def write_csv_file(stat_data: Dict) -> None:
+    filed_names = list(stat_data.keys())
+    with open("curry_data.csv", "w") as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=filed_names)
+        writer.writeheader()
+        writer.writerow(stat_data)
 
-a = get_stat_data("pts")
-print("PTS: ", a)
-print("PTS average: ", sum(a)/len(a))
 
-a = get_stat_data("fg3")
-print("3pa: ", a)
-print("3pa average:: ", sum(a)/len(a))
+def main():
+    curry_profile = requests.get(
+        "https://www.basketball-reference.com/players/c/curryst01/gamelog/2021"
+    )
+    stat_data = get_stat_data(curry_profile)
+    # write_csv_file(stat_data)
+
+
+if __name__ == "__main__":
+    main()
